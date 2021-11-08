443050e58bbf7014d120d4b03a0348b8d511b3e6
Brandt <derrickb@la1124.lib.iastate.edu>
2021-11-08 16:06:38 -0600

    deleting non needed files. They were added to components folder.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import GameContainer from './components/GameContainer';
 import pongSrc from "./assets/Images/Pong.png"
 import froggerSrc from "./assets/Images/Frogger.jpg"
 import asteroidsSrc from "./assets/Images/Asteroids.jpg"
-
+import Footer from './components/Footer';
+import Button from './components/Button';
+import SignupPage from './components/SignupPage';
+import LoginPage from './components/LoginPage';
+import { Outlet, Link } from 'react-router-dom';
 
 function App() {
 
@@ -14,13 +19,17 @@ function App() {
   const pongTitle = "Pong"
   const froggerTitle = "Frogger"
   const asteroidsTitle = "Asteroids"
+  
   return (
-    <div>
-      <Header />
-      <GameContainer imgSrc={pongSrc} gameTitle={pongTitle} />
-      <GameContainer imgSrc={froggerSrc} gameTitle={froggerTitle}/>
-      <GameContainer imgSrc={asteroidsSrc} gameTitle={asteroidsTitle}/>
-    </div>
+      <div>
+        <Header buttonText="Login"/>
+        <GameContainer imgSrc={pongSrc} gameTitle={pongTitle} />
+        <GameContainer imgSrc={froggerSrc} gameTitle={froggerTitle}/>
+        <GameContainer imgSrc={asteroidsSrc} gameTitle={asteroidsTitle}/>
+        <Button buttonText="High Scores" />
+        <Footer />
+        <Link to="/signup">Link</Link>
+      </div>
   );
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,14 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import SignupPage from "./components/SignupPage"
+import LoginPage from "./components/LoginPage"
+import PongPage from "./components/PongPage"
+import SnakePage from "./components/SnakePage"
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="signup" element={<SignupPage />} />
+      <Route path="login" element={<LoginPage />} />
+      <Route path="highscore" element={<LoginPage />} />
+      <Route path="pong" element={<PongPage />} />
+      <Route path="snake" element={<SnakePage />} />
+    </Routes>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitalsx
