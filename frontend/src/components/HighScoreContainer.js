530d7fe6acfd02588a9bfb3f1a640caaacb1a910
Brandt <derrickb@la1124.lib.iastate.edu>
2021-11-10 10:43:51 -0600

    Reoriented logic to make use of state variables for stat page. Base logic is installed, and it Will be able to load data in from server based on user click on particular game.

--- frontend/src/components/HighScoreContainer.js
+++ frontend/src/components/HighScoreContainer.js
@@ -5,7 +5,11 @@ import ScoreContainer from "./ScoreContainer"
 import RankContainer from "./RankContainer"
 import GameTabs from "./GameTabs"
 
-const HighScoreContainer = () => {
+const HighScoreContainer = ({stats}) => {
+
+    console.log(stats)
+    console.log(stats.Name)
+
     return (
         <>
         <h4 className="highScoreTitle">Total Score Ranking</h4>
@@ -14,17 +18,15 @@ const HighScoreContainer = () => {
         
                 <div className="column">
                     <h4 className="columnTitle">Name</h4>
-                    <NameContainer />
+                    <NameContainer names={stats.Name} />
                 </div>
                 <div className="column"> 
                     <h4 className="columnTitle">Score</h4>
-                    <ScoreContainer />
+                    <ScoreContainer scores={stats.Score} />
                 </div>
                 <div className="column">
-                    <h4 className="columnTitle">
-                        Rank
-                    </h4>
-                    <RankContainer />
+                    <h4 className="columnTitle">Rank</h4>
+                    <RankContainer ranks={stats.Rank} />
                 </div>
             
         </div>
