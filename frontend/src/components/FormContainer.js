a640e83cf5b91cfdbd4bb7814cd3ffe2945853dc
Derrick Brandt <derrickbrandt@la1190.lib.iastate.edu>
2021-11-17 17:06:09 -0600

    adds the beginning logic for submit handling on signup page.

--- frontend/src/components/FormContainer.js
+++ frontend/src/components/FormContainer.js
@@ -1,10 +1,24 @@
 // import React from 'react'
+import { useState } from "react"
 import { Link } from "react-router-dom"
 
 const FormContainer = ({hideSignupBtn}) => {
+
+    //need a state component here...??
+    const [username, setUsername] = useState('')
+ 
+    const handleSubmit = event => {
+        // event.preventDefault()
+    
+        // axios.get(`${username}`).then(resp => {
+        //   props.onSubmit(resp.data)
+        //   setUsername('')
+        // }) #TODO
+      }
+
     return (
         <div className="formContainer">
-            <form action="">
+            <form onSubmit="">
                 <label htmlFor="userName">Username:</label>
                 <input type="text" id="user_name" name="user_name" />
                 <label htmlFor="password">Password:</label>
@@ -12,7 +26,7 @@ const FormContainer = ({hideSignupBtn}) => {
                 <Link to="/signup">
                 <button className="signup-btn">Signup</button>
                 </Link>
-                <button hidden={hideSignupBtn} className="login-btn">Login</button>
+                <button type="submit" hidden={hideSignupBtn} className="login-btn">Login</button>
             </form>
         </div>
     )
