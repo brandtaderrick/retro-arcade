99f66f40d4716edac9466065519a8cf0f393d3c2
Logan Schmit <lschmit@iastate.edu>
2021-11-27 18:13:24 -0600

    add inititial UserStatsPage

---
+++ client/src/components/UserStatsPage.js
@@ -0,0 +1,16 @@
+//import React from 'react'
+
+import { useState, useEffect } from 'react';
+import Header from "./Header"
+import HighScoreContainer from "./HighScoreContainer"
+import GameTabs from "./GameTabs"
+
+const UserStatsPage = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default UserStatsPage
