2fd6a447fefc5b20165f9a5139ce727aff444d53
Derrick <derrickb@iastate.edu>
2021-12-02 23:49:34 -0600

    adds beginning changes for login button changes

--- client/src/components/LoginPage.js
+++ client/src/components/LoginPage.js
@@ -3,21 +3,31 @@ import { useState, useEffect } from "react"
 import Header from "./Header"
 import FormContainer from "./FormContainer"
 
-const LoginPage = ({buttonText}) => {
+const LoginPage = ({buttonText, parentRef}) => {
 
-    const [textFromLogin, setTextFromLogin] = useState('')
+    const [textFromLogin, setTextFromLogin] = useState(buttonText)
+
+    // const [buttonText, setButtonText] = useState("Login");
+
+    // useEffect(() => {
+    // console.log(buttonText)
+    // },[buttonText])
 
     const handleLogin = (userNameText) => {
         setTextFromLogin(userNameText);
     }
     
+    const propogateLoginText = () => {
+
+    }
+
     useEffect(() => {
-        console.log(textFromLogin);
+        
     }, [textFromLogin])
 
     return (
         <div>
-            <Header displayButton={true} buttonText={buttonText}/>
+            <Header displayButton={false} buttonText={textFromLogin}/>
             <h2 className="loginHeader"> Login </h2>
             {/* #TODO Implement FormContainer and place here */}
             <FormContainer parentRef={handleLogin} hideSignupBtn={false}/>
