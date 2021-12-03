1c425b5be388177e265c4cd2ef698101ae987f9f
Derrick <derrickb@iastate.edu>
2021-12-03 12:22:57 -0600

    adds button with logic for persistent user

--- client/src/components/AsteroidsPage.js
+++ client/src/components/AsteroidsPage.js
@@ -1,10 +1,18 @@
 // import React from 'react'
+import { useState } from "react"
 import Header from "./Header"
 
-const AsteroidsPage = () => {
+
+const AsteroidsPage = ({buttonText}) => {
+
+    const userStatsLink = "/userStats"
+    const loginLink = "/login"
+    var  [btnText, setBtnText] = useState(buttonText)
+
     return (
         <div>
-            <Header />
+            <Header displayButton={true} pLink={localStorage.getItem("userName") ? userStatsLink : loginLink} buttonText={btnText} />
+            Under Development
         </div>
     )
 }
