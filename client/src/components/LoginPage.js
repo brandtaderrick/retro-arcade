e5c471a32ccea6e1f3dffc162be6ad782d4a4a80
Derrick <derrickb@iastate.edu>
2021-12-03 13:50:11 -0600

    starts implementing score/life tracking on Pong Page

--- client/src/components/LoginPage.js
+++ client/src/components/LoginPage.js
@@ -17,10 +17,6 @@ const LoginPage = ({buttonText, parentRef}) => {
         setTextFromLogin(userNameText);
         parentRef(userNameText)
     }
-    
-    const propogateLoginText = () => {
-
-    }
 
     useEffect(() => {
         
