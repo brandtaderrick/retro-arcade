4c4c0957a8199d8b28019854692a18c17580e28c
Luke <lthunga@iastate.edu>
2021-12-05 13:43:37 -0600

    added mongoose connection:

--- server/app.js
+++ server/app.js
@@ -1,3 +1,15 @@
+const mongoose = require('mongoose');
+
+// Connect to mongodb
+mongoose.connect('mongodb://localhost:27017/myapp');
+
+mongoose.connection.once('open', function(){
+  console.log('Connection has been made.');
+}).on('error', function(error){
+  console.log('Connection error:', error);
+});
+
+
 const http = require('http')
 const fs = require('fs')
 const port = 5000
