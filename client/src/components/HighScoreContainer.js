8e0d24baa2a30aaf6ab423c6c49ea66b869d2681
Derrick <derrickb@iastate.edu>
2021-12-04 18:56:57 -0600

    provides base logic for dynamic render of data based on click choice

--- client/src/components/HighScoreContainer.js
+++ client/src/components/HighScoreContainer.js
@@ -1,5 +1,5 @@
 // import React from 'react'
-
+import { useState } from "react"
 import NameContainer from "./NameContainer"
 import ScoreContainer from "./ScoreContainer"
 import RankContainer from "./RankContainer"
@@ -8,19 +8,29 @@ import GameTabs from "./GameTabs"
 const HighScoreContainer = ({stats}) => {
 
     console.log(stats)
-    console.log(stats.Name)
+
+    // useState for Pong, Snake, etc.
+    const [pongNamesData, setPongNamesData] = useState(null)
+    const [snakeData, setSnakeData] = useState(null)
 
     const handleGameClick = (e) => {
-        console.log(e.target.textContent)
+        // base logic for dynamically rendering data based on game click
+        if(e.target.textContent === "Pong"){
+            console.log(stats.Pong.Name)
+        }
+        if(e.target.textContent === "Snake"){
+            console.log(stats.Snake.Name)
+        }
     }
 
     return (
         <>
         <h4 className="highScoreTitle">Total Score Ranking</h4>
         <GameTabs parentRef={handleGameClick}/>
+
         <div className="highScoreContainer">
         
-                <div className="column">
+                {/* <div className="column">
                     <h4 className="columnTitle">Name</h4>
                     <NameContainer names={stats.Name} />
                 </div>
@@ -31,7 +41,7 @@ const HighScoreContainer = ({stats}) => {
                 <div className="column">
                     <h4 className="columnTitle">Rank</h4>
                     <RankContainer ranks={stats.Rank} />
-                </div>
+                </div> */}
         </div>
 
         </>
